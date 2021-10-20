adce5bc685ead63ab03964360c7ade084a9ded8e
shafi <shafi@douran.com>
2021-10-20 01:12:51 -0700

    Add service of person

---
+++ src/main/java/com/example/shesh/Services/PersonService.java
@@ -0,0 +1,45 @@
+package com.example.shesh.Services;
+
+import com.example.shesh.Models.Person;
+import com.example.shesh.Repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    PersonRepository repository;
+
+    public Person getPersonById(Long id) {
+        return repository.findById(id.intValue()).get();
+    }
+
+    public ArrayList<Person> getPersons() {
+        ArrayList<Person> personList = new ArrayList<>();
+        repository.findAll().forEach(person -> personList.add(person));
+        return personList;
+    }
+
+    public Person setPerson(Person person) {
+        return repository.save(person);
+    }
+
+    public Person updatePerson(Person person) {
+        return repository.save(person);
+    }
+
+    public void deletePersonById(Long id) {
+        repository.deleteById(id.intValue());
+    }
+
+    public void deleteAllPersons() {
+        repository.deleteAll();
+    }
+
+    public void deletePerson(Person person) {
+        repository.delete(person);
+    }
+}
