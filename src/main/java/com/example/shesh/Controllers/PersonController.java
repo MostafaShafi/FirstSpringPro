996cfef3e30c19f18e7196f3cd230193c70db597
shafi <shafi@douran.com>
2021-10-20 01:12:23 -0700

    Add controller of person

---
+++ src/main/java/com/example/shesh/Controllers/PersonController.java
@@ -0,0 +1,41 @@
+package com.example.shesh.Controllers;
+
+import com.example.shesh.Models.Person;
+import com.example.shesh.Services.PersonService;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+public class PersonController {
+
+    @Autowired
+    PersonService service;
+    @Autowired
+    private Gson gson;
+
+    @PostMapping("/addperson")
+    public Person addPerson(@Valid @RequestBody Person person, BindingResult result) {
+        return service.setPerson(person);
+    }
+
+    @PutMapping("/editperson/{id}")
+    public Person updatePerson(@PathVariable(value = "id") Long id, @Valid @RequestBody Person person) {
+        return service.updatePerson(person);
+    }
+
+    @GetMapping("/person/{id}")
+    public String getPerson(@PathVariable(value = "id") Long id) {
+        return gson.toJson(service.getPersonById(id));
+    }
+
+    @GetMapping("/persons")
+    public String getPersons() {
+        return gson.toJsonTree(service.getPersons()).toString();
+    }
+
+
+}
