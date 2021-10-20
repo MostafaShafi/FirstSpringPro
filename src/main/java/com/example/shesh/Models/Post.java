20fda6e7fad145b829a9cdda148f529cd28da097
shafi <shafi@douran.com>
2021-10-20 00:12:13 -0700

    Edit models of post

--- src/main/java/com/example/shesh/Models/Post.java
+++ src/main/java/com/example/shesh/Models/Post.java
@@ -5,6 +5,8 @@ import com.google.gson.annotations.SerializedName;
 import javax.persistence.*;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import javax.xml.crypto.Data;
+import java.util.Date;
 
 @Entity
 @Table(name = "post", schema = "public")
@@ -12,68 +14,83 @@ public class Post {
     @Id
     @Column(name = "id")
     @SerializedName("id")
-    private Integer _id;
-    @Column(name = "user_id")
-    @Size(max = 2, min = 2)
-    @Pattern(regexp = "[0-9]{2}", message = "Should be just 2 digits!")
-    @SerializedName("userId")
-    private Integer _userId;
+    private Long _id;
+
+    @Column(name = "author")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Author _author;
+
     @Column(name = "title")
     @SerializedName("title")
     private String _title;
+
     @Column(name = "body")
     @SerializedName("body")
     private String _body;
 
+    @Column(name = "releaseDate")
+    @Temporal(TemporalType.DATE)
+    private Date _releaseDate;
+
+    @Column(name = "lastUpdate")
+    @Temporal(TemporalType.DATE)
+    private Date _lastUpdate;
+
     public Post() {
     }
 
-    public Post(Integer id, Integer userId, String title, String body) {
-        this._id = id;
-        this._userId = userId;
-        this._title = title;
-        this._body = body;
+    public Post(Long _id, Author _author, String _title, String _body) {
+        this._id = _id;
+        this._author = _author;
+        this._title = _title;
+        this._body = _body;
     }
 
-    public Integer get_id() {
+    public Long getId() {
         return _id;
     }
 
-    public void set_id(Integer _id) {
+    public void setId(Long _id) {
         this._id = _id;
     }
 
-    public Integer get_userId() {
-        return _userId;
+    public Author getAuthor() {
+        return _author;
     }
 
-    public void set_userId(Integer userId) {
-        this._userId = userId;
+    public void setAuthor(Author _author) {
+        this._author = _author;
     }
 
-    public String get_title() {
+    public String getTitle() {
         return _title;
     }
 
-    public void set_title(String title) {
-        this._title = title;
+    public void setTitle(String _title) {
+        this._title = _title;
     }
 
-    public String get_body() {
+    public String getBody() {
         return _body;
     }
 
-    public void set_body(String body) {
-        this._body = body;
+    public void setBody(String _body) {
+        this._body = _body;
     }
 
-    @Override
-    public String toString() {
-        return "Post{" +
-                "id=" + _id +
-                ", userId=" + _userId +
-                ", title='" + _title + '\'' +
-                ", body='" + _body + '\'' +
-                '}';
+    public Date getReleaseDate() {
+        return _releaseDate;
+    }
+
+    public void setReleaseDate(Date _releaseDate) {
+        this._releaseDate = _releaseDate;
+    }
+
+    public Date getLastUpdate() {
+        return _lastUpdate;
+    }
+
+    public void setLastUpdate(Date _lastUpdate) {
+        this._lastUpdate = _lastUpdate;
     }
 }
