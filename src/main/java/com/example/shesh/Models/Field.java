21d4709ea0dadf357e54e61d7a5a2f4f612d98a6
MostafaShafi <mostafa.shafi94@gmail.com>
2021-12-28 19:30:51 +0330

    Add fields and constracture to model

--- src/main/java/com/example/shesh/Models/Field.java
+++ src/main/java/com/example/shesh/Models/Field.java
@@ -1,38 +1,49 @@
 package com.example.shesh.Models;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.google.gson.annotations.SerializedName;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "Field")
 public class Field {
     @Id
-    private Integer id;
-    private Integer name;
-    private Integer gradeId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SerializedName("id")
+    @Column(name = "id")
+    private Integer _id;
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SerializedName("name")
+    @Column(name = "name")
+    private Integer _name;
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SerializedName("gradeId")
+    @Column(name = "gradeId")
+    private Integer _gradeId;
 
     public Integer getId() {
-        return id;
+        return _id;
     }
 
     public void setId(Integer id) {
-        id = id;
+        this._id = id;
     }
 
     public Integer getName() {
-        return name;
+        return _name;
     }
 
     public void setName(Integer name) {
-        this.name = name;
+        this._name = name;
     }
 
     public Integer getGradeId() {
-        return gradeId;
+        return _gradeId;
     }
 
     public void setGradeId(Integer gradeId) {
-        this.gradeId = gradeId;
+        this._gradeId = gradeId;
     }
 }
